3bb623063b8adb80f16e7ae3b75525ca4f6bbfbe
Lucas Archanjo <lucasarchanjo1010@gmail.com>
2024-07-31 10:26:55 -0300

    [fix] max-width issue solved

--- arkh-slider.js
+++ arkh-slider.js
@@ -1,241 +1,162 @@
 const selector = () => document.querySelector(arkhSlider().target);
-
 function createSlider() {
-  const baseConfig = arkhSlider();
-  let config = { ...baseConfig };
-
-  const applyMobileConfig = () => {
-    if (window.matchMedia("(max-width: 767px)").matches) {
-      config = { ...baseConfig, ...baseConfig.mobile };
-    } else {
-      config = { ...baseConfig };
+  const e = arkhSlider();
+  let t = { ...e };
+  const n = () => {
+      (t = window.matchMedia("(max-width: 767px)").matches ? { ...e, ...e.mobile } : { ...e }), c();
+    },
+    l = document.querySelector(arkhSlider().target);
+  if (!l) return;
+  const s = l.style;
+  s.position = "relative";
+  const r = Array.from(l.children).filter((e) => !["prev", "next", "dots"].some((t) => e.classList.contains(t)));
+  let o,
+    a = 0,
+    i = !1,
+    d = 0;
+  const c = () => {
+    const { itensToShow: e, interval: n, infinite: c, autoplay: h, arrows: y, dots: u } = t;
+    if (((l.innerHTML = ""), r.forEach((e) => l.appendChild(e)), y)) {
+      const t = createButton("❯", "absolute", "10px", "50%", "right"),
+        n = createButton("❮", "absolute", "10px", "50%", "left");
+      l.appendChild(t),
+        l.appendChild(n),
+        t.addEventListener("click", () => {
+          (c || a + e < r.length) && ((a = (a + 1) % r.length), g());
+        }),
+        n.addEventListener("click", () => {
+          (c || a > 0) && ((a = (a - 1 + r.length) % r.length), g());
+        });
     }
-    updateSliderConfig();
+    let p = [];
+    if (u) {
+      const e = document.createElement("div");
+      e.classList.add("dots"),
+        (e.style.position = "absolute"),
+        (e.style.bottom = "10px"),
+        (e.style.left = "50%"),
+        (e.style.transform = "translateX(-50%)"),
+        (p = r.map((t, n) => {
+          const l = document.createElement("span");
+          return (
+            (l.style.display = "inline-block"),
+            (l.style.width = "10px"),
+            (l.style.height = "10px"),
+            (l.style.borderRadius = "50%"),
+            (l.style.margin = "0 5px"),
+            (l.style.cursor = "pointer"),
+            (l.style.backgroundColor = "rgba(255, 255, 255, 0.5)"),
+            l.addEventListener("click", () => {
+              (a = n), g();
+            }),
+            e.appendChild(l),
+            l
+          );
+        })),
+        l.appendChild(e);
+    }
+    (s.display = "flex"),
+      (s.overflow = "hidden"),
+      (s.width = "100%"),
+      r.forEach((t, n) => {
+        const l = t.style;
+        (l.maxWidth = 100 / e + "%"), (l.padding = "4px");
+        t.querySelectorAll("img").forEach((e) => {
+          (e.style.width = "100%"),
+            e.addEventListener("mousedown", (e) => {
+              e.preventDefault();
+            });
+        }),
+          n >= e && (t.style.display = "none");
+      });
+    const g = () => {
+        r.forEach((e, t) => {
+          (e.style.display = "none"), (e.style.order = (t - a + r.length) % r.length);
+        });
+        for (let t = 0; t < e; t++) {
+          let e = a + t;
+          c && (e = (a + t) % r.length), (r[e].style.display = "flex"), (r[e].style.order = t);
+        }
+        f();
+      },
+      f = () => {
+        p.length > 0 &&
+          p.forEach((e, t) => {
+            e.style.backgroundColor = t === a ? "rgba(0, 0, 0, 0.5)" : "rgba(151, 151, 151, 0.5)";
+          });
+      };
+    n > 0 &&
+      h &&
+      (o && clearInterval(o),
+      (o = setInterval(() => {
+        (c || a + e < r.length) && ((a = (a + 1) % r.length), g(), !c && a + e >= r.length && clearInterval(o));
+      }, n)));
+    const m = (e) => {
+        (i = !0),
+          (d = e.type.includes("mouse") ? e.pageX : e.touches[0].clientX),
+          l.classList.add("grabbing"),
+          r.forEach((e) => {
+            e.style.cursor = "pointer";
+          });
+      },
+      v = (t) => {
+        if (i) {
+          r.forEach((e) => {
+            e.style.cursor = "grabbing";
+          });
+          const n = (t.type.includes("mouse") ? t.pageX : t.touches[0].clientX) - d;
+          Math.abs(n) > 50 &&
+            (n < 0 && (c || a + e < r.length)
+              ? (a = (a + 1) % r.length)
+              : n > 0 && (c || a > 0) && (a = (a - 1 + r.length) % r.length),
+            g(),
+            (i = !1));
+        }
+      },
+      E = () => {
+        (i = !1),
+          l.classList.remove("grabbing"),
+          r.forEach((e) => {
+            e.style.cursor = "pointer";
+          });
+      };
+    l.addEventListener("mousedown", m),
+      l.addEventListener("touchstart", m),
+      l.addEventListener("mousemove", v),
+      l.addEventListener("touchmove", v),
+      l.addEventListener("mouseup", E),
+      l.addEventListener("touchend", E),
+      l.addEventListener("mouseleave", () => {
+        i && E();
+      }),
+      l.querySelectorAll("a").forEach((e) => {
+        e.addEventListener("click", (e) => {
+          i && e.preventDefault();
+        });
+      }),
+      g();
   };
-
-  const targetElement = selector();
-
-  if (!targetElement) {
-    console.error("Target element not found");
-    return;
-  }
-
-  const targetStyle = targetElement.style;
-  targetStyle.position = "relative";
-
-  const children = Array.from(targetElement.children).filter((child) => {
-    return !["prev", "next", "dots"].some((className) => child.classList.contains(className));
-  });
-
-  let currentIndex = 0;
-  let isDragging = false;
-  let startX = 0;
-  let currentTranslate = 0;
-  let prevTranslate = 0;
-  let animationID = 0;
-  let intervalId;
-
-  const updateSliderConfig = () => {
-    const { itensToShow, interval, infinite, autoplay, arrows, dots } = config;
-
-    targetElement.innerHTML = "";
-    children.forEach((child) => targetElement.appendChild(child));
-
-    if (arrows) {
-      const nextButton = createButton("❯", "absolute", "10px", "50%", "right");
-      const prevButton = createButton("❮", "absolute", "10px", "50%", "left");
-
-      targetElement.appendChild(nextButton);
-      targetElement.appendChild(prevButton);
-
-      nextButton.addEventListener("click", () => {
-        if (infinite || currentIndex + itensToShow < children.length) {
-          currentIndex = (currentIndex + 1) % children.length;
-          updateSlider();
-        }
-      });
-
-      prevButton.addEventListener("click", () => {
-        if (infinite || currentIndex > 0) {
-          currentIndex = (currentIndex - 1 + children.length) % children.length;
-          updateSlider();
-        }
-      });
-    }
-
-    let dotElements = [];
-    if (dots) {
-      const dotsContainer = document.createElement("div");
-      dotsContainer.classList.add("dots");
-      dotsContainer.style.position = "absolute";
-      dotsContainer.style.bottom = "10px";
-      dotsContainer.style.left = "50%";
-      dotsContainer.style.transform = "translateX(-50%)";
-
-      dotElements = children.map((_, index) => {
-        const dot = document.createElement("span");
-        dot.style.display = "inline-block";
-        dot.style.width = "10px";
-        dot.style.height = "10px";
-        dot.style.borderRadius = "50%";
-        dot.style.margin = "0 5px";
-        dot.style.cursor = "pointer";
-        dot.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
-
-        dot.addEventListener("click", () => {
-          currentIndex = index;
-          updateSlider();
-        });
-
-        dotsContainer.appendChild(dot);
-        return dot;
-      });
-
-      targetElement.appendChild(dotsContainer);
-    }
-
-    targetStyle.display = "flex";
-    targetStyle.overflow = "hidden";
-    targetStyle.width = "100%";
-
-    children.forEach((child, index) => {
-      const childStyle = child.style;
-      childStyle.minWidth = `${100 / itensToShow}%`;
-      childStyle.padding = "4px";
-      const images = child.querySelectorAll("img");
-      images.forEach((image) => {
-        image.style.width = "100%";
-        image.addEventListener("mousedown", (event) => {
-          event.preventDefault();
-        });
-      });
-
-      if (index >= itensToShow) {
-        child.style.display = "none";
-      }
-    });
-
-    const updateSlider = () => {
-      children.forEach((child, index) => {
-        child.style.display = "none";
-        child.style.order = (index - currentIndex + children.length) % children.length;
-      });
-
-      for (let i = 0; i < itensToShow; i++) {
-        let showIndex = currentIndex + i;
-        if (infinite) {
-          showIndex = (currentIndex + i) % children.length;
-        }
-        children[showIndex].style.display = "flex";
-        children[showIndex].style.order = i;
-      }
-
-      highlightDot();
-    };
-
-    const highlightDot = () => {
-      if (dotElements.length > 0) {
-        dotElements.forEach((dot, index) => {
-          dot.style.backgroundColor = index === currentIndex ? "rgba(0, 0, 0, 0.5)" : "rgba(151, 151, 151, 0.5)";
-        });
-      }
-    };
-
-    if (interval > 0 && autoplay) {
-      if (intervalId) clearInterval(intervalId);
-      intervalId = setInterval(() => {
-        if (infinite || currentIndex + itensToShow < children.length) {
-          currentIndex = (currentIndex + 1) % children.length;
-          updateSlider();
-          if (!infinite && currentIndex + itensToShow >= children.length) {
-            clearInterval(intervalId);
-          }
-        }
-      }, interval);
-    }
-
-    const startDragging = (event) => {
-      isDragging = true;
-      startX = event.type.includes("mouse") ? event.pageX : event.touches[0].clientX;
-      targetElement.classList.add("grabbing");
-
-      children.forEach((child) => {
-        child.style.cursor = "pointer";
-      });
-    };
-
-    const dragging = (event) => {
-      if (isDragging) {
-        children.forEach((child) => {
-          child.style.cursor = "grabbing";
-        });
-        const currentPosition = event.type.includes("mouse") ? event.pageX : event.touches[0].clientX;
-        const movedBy = currentPosition - startX;
-
-        if (Math.abs(movedBy) > 50) {
-          if (movedBy < 0 && (infinite || currentIndex + itensToShow < children.length)) {
-            currentIndex = (currentIndex + 1) % children.length;
-          } else if (movedBy > 0 && (infinite || currentIndex > 0)) {
-            currentIndex = (currentIndex - 1 + children.length) % children.length;
-          }
-          updateSlider();
-          isDragging = false;
-        }
-      }
-    };
-
-    const stopDragging = () => {
-      isDragging = false;
-      targetElement.classList.remove("grabbing");
-      children.forEach((child) => {
-        child.style.cursor = "pointer";
-      });
-    };
-
-    targetElement.addEventListener("mousedown", startDragging);
-    targetElement.addEventListener("touchstart", startDragging);
-    targetElement.addEventListener("mousemove", dragging);
-    targetElement.addEventListener("touchmove", dragging);
-    targetElement.addEventListener("mouseup", stopDragging);
-    targetElement.addEventListener("touchend", stopDragging);
-    targetElement.addEventListener("mouseleave", () => {
-      if (isDragging) stopDragging();
-    });
-
-    targetElement.querySelectorAll("a").forEach((link) => {
-      link.addEventListener("click", (event) => {
-        if (isDragging) {
-          event.preventDefault();
-        }
-      });
-    });
-
-    updateSlider();
-  };
-
-  applyMobileConfig();
-  window.addEventListener("resize", applyMobileConfig);
+  n(), window.addEventListener("resize", n);
 }
-
-function createButton(text, position, rightOrLeft, top, direction) {
-  const button = document.createElement("div");
-  button.innerText = text;
-  button.style.position = position;
-  button.style[direction] = rightOrLeft;
-  button.style.top = top;
-  button.style.height = "24px";
-  button.style.width = "24px";
-  button.style.display = "flex";
-  button.style.alignItems = "center";
-  button.style.justifyContent = "center";
-  button.style.borderRadius = "50%";
-  button.style.cursor = "pointer";
-  button.style.fontSize = "20px";
-  button.style.fontWeight = "bold";
-  button.style.userSelect = "none";
-  button.style.color = "#808080";
-
-  return button;
+function createButton(e, t, n, l, s) {
+  const r = document.createElement("div");
+  return (
+    (r.innerText = e),
+    (r.style.position = t),
+    (r.style[s] = n),
+    (r.style.top = l),
+    (r.style.height = "24px"),
+    (r.style.width = "24px"),
+    (r.style.display = "flex"),
+    (r.style.alignItems = "center"),
+    (r.style.justifyContent = "center"),
+    (r.style.borderRadius = "50%"),
+    (r.style.cursor = "pointer"),
+    (r.style.fontSize = "20px"),
+    (r.style.fontWeight = "bold"),
+    (r.style.userSelect = "none"),
+    (r.style.color = "#808080"),
+    r
+  );
 }
-
 createSlider();
